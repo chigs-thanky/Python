2c59094558d491836142c09b958b7aadd5cc77ae
chigs-thanky <chigsthanky@outlook.com>
2026-05-16 12:16:55 +0530

    Assign function variable

---
+++ Assign_Fucntion_to_Variable.py
@@ -0,0 +1,10 @@
+#Define function
+def msg(name):
+    return f"Hello, {name}!"
+    
+#Assign the function to a variable
+txt = msg
+
+#Call function using the variable
+print(txt("Chirag"))
+print(txt("Dee"))
