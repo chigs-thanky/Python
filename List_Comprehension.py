d77efc1cd8d49e8469f6009fe3d990c75b9f9c77
chigs-thanky <chigsthanky@outlook.com>
2026-06-04 12:45:40 +0530

    List Comprehension

---
+++ List_Comprehension.py
@@ -0,0 +1,7 @@
+words = ["python", "list", "comprehension"]
+unique_1_L = [lt[0].upper() for lt in words]
+print(unique_1_L)
+
+name = ["jack", "rose"]
+fLetter = [d[0].upper() for d in name]
+print(f"{fLetter[0]} loves {fLetter[1]}")
